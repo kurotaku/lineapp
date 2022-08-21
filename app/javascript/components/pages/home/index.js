75804e1dfc2bedaa3df848f40aa9457aa45f40e6
kurotaku <kuroiwa.taku@gmail.com>
2022-08-22 08:48:51 +0900

    LINE疎通

--- app/javascript/components/pages/home/index.js
+++ app/javascript/components/pages/home/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import liff from '@line/liff'
+import { CurrentLineAccount } from '../../App'
 
 const Home = () => {
+  const currentLineAccount = useContext(CurrentLineAccount);
   return (
     <div>
       this is from react.
+      {currentLineAccount.name && <p>ようこそ {currentLineAccount.name} さん</p>}
+      <button onClick={logout}>ログアウト</button>
     </div>
   )
 }
-
 export default Home
+
+const logout = () => {
+  liff.logout();
+  location.reload();
+}
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'site#home'
+
+  namespace :api do
+    namespace :private do
+      resources :line_accounts, only: %w[create]
+    end
+  end
 end
